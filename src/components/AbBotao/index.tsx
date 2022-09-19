c7e47797cc2192c027dfeffc329fb0cfa4c391b8
Adriano <adriano_alves_2008@hotmail.com>
2022-09-19 10:25:39 -0300

    estilizando o button

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const BotaoEstilizado = styled.button `
+  background: #eb9800;
+  padding: 16px 32px;
+  border: 2px solid #eb9800 ;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+    background: #b87900;
+    border: 2px solid #b87900 ;
+  }
+`
 
 function AbBotao() {
   return (
-    <button>Clique aqui</button>
+    <BotaoEstilizado>Clique aqui</BotaoEstilizado>
   )
 }
 
