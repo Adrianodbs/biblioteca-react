bfad8e377d8fca6dccc8eb19289e2ae601365921
Adriano <adriano_alves_2008@hotmail.com>
2022-09-22 08:56:37 -0300

    componentes mais complexos

---
+++ src/components/AbGrupoOpcoes/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export function AbGrupoOpcoes() {
+  return (
+    <>
+    <section>
+      <header>
+        E-book
+      </header>
+      <div>
+        <strong>R$ 00,00</strong>
+      </div>
+      <footer>
+        .pdf, .pub, .mobi
+      </footer>
+    </section>
+
+    <section>
+      <header>
+        E-book
+      </header>
+      <div>
+        <strong>R$ 00,00</strong>
+      </div>
+      <footer>
+        .pdf, .pub, .mobi
+      </footer>
+    </section>
+    </>
+
+  )
+}
---
+++ stories/AbGrupoOpcoes.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {AbGrupoOpcoes} from '../src/components/AbGrupoOpcoes'
+
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+
+
+export default {
+  title: 'Componentes/AbGrupoOpcoes',
+  component: AbGrupoOpcoes
+} as ComponentMeta <typeof AbGrupoOpcoes>
+
+const Template: ComponentStory<typeof AbGrupoOpcoes> = () => <AbGrupoOpcoes />
+
+export const Padrao = Template.bind({})
+
+Padrao.args ={}
